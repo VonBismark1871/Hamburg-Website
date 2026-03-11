e5d1d375fb1169471bfe7ae24e5737b18cab1e10
Andrii Oleksiienko <76771947+VonBismark1871@users.noreply.github.com>
2026-03-11 19:09:46 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/brand/favicon-hw-approved-v1.png?v=20260311c" type="image/png" />
-        <link rel="shortcut icon" href="/brand/favicon-hw-approved-v1.png?v=20260311c" type="image/png" />
-        <link rel="apple-touch-icon" href="/brand/favicon-hw-approved-v1.png?v=20260311c" />
+        <link rel="icon" href="/brand/hw-approved-icon-transparent-v1.png" type="image/png" />
+        <link rel="shortcut icon" href="/brand/hw-approved-icon-transparent-v1.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/brand/hw-approved-icon-transparent-v1.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
