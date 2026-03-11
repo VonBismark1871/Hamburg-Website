d5ee490f75c851714381c6ff880a47f46388c689
Andrii Oleksiienko <76771947+VonBismark1871@users.noreply.github.com>
2026-03-11 18:46:52 +0100

    Update favicon links in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/brand/favicon-v3.svg?v=20260311b" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/brand/favicon-v3.svg?v=20260311b" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/brand/favicon-v3.svg?v=20260311b" />
+        <link rel="icon" href="/brand/favicon-hw-approved-v1.png?v=20260311c" type="image/png" />
+        <link rel="shortcut icon" href="/brand/favicon-hw-approved-v1.png?v=20260311c" type="image/png" />
+        <link rel="apple-touch-icon" href="/brand/favicon-hw-approved-v1.png?v=20260311c" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
